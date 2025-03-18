f5bcd38aa201e23758b47855ed17a9fb45f319cf
guilherme <guilhermev2huehue@gmail.com>
2025-03-18 15:22:18 -0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        pythonEnv = pkgs.python311.withPackages (ps: with ps; [
+          jupyterlab
+          numpy
+          pandas
+          pyarrow
+          matplotlib
+          scikit-learn
+          seaborn
+        ]);
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gcc
+            cmake
+            gnumake 
+            root 
+            arrow-cpp
+            pythonEnv
+          ];
+
+          shellHook = ''
+            export JUPYTER_CONFIG_DIR="$HOME/.jupyter"
+            export JUPYTER_DATA_DIR="$HOME/.local/share/jupyter"
+            mkdir -p "$JUPYTER_CONFIG_DIR"
+            mkdir -p "$JUPYTER_DATA_DIR"
+            echo "Welcome to the development environment!"
+          '';
+        };
+      });
+}
